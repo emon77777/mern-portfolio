93985f3eab11d8f999e7b5f83a2287da7c7d77a7
Emon <emon77777@gmail.com>
2023-01-25 22:42:23 +0600

    http added.

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,12 @@
-const express = require('express'); //Import the express dependency
-const app = express();              //Instantiate an express app, the main work horse of this server
-const port = 5000;                  //Save the port number where your server will be listening
+var http = require("http");
+const express = require('express');
+const app = express();
+const port = 5000;
 
-//Idiomatic expression in express to route and respond to a client request
-app.get('/', (req, res) => {        //get requests to the root ("/") will route here
-    res.sendFile('index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
-                                                        //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
+app.get('/', (req, res) => {
+    res.sendFile('index.html', {root: __dirname});
 });
 
-app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
+app.listen(port, () => { 
     console.log(`Now listening on port ${port}`); 
 });
